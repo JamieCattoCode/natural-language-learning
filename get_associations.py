a017e71f66f47030a1b3044e91b8010c619f1c26
JamieCattoCode <jamietcatto@gmail.com>
2024-02-21 17:49:53 -0300

    Added assistant ID

--- get_associations.py
+++ get_associations.py
@@ -5,9 +5,9 @@ import os
     # Set up OpenAI key
     # Use assistant ID to retrieve the assistant
 
-# Check for OpenAI API key
 assert os.environ.get('OPENAI_API_KEY'), "OPENAI_API_KEY not found in .env file."
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+ASSISTANT_ID = os.environ.get('ASSISTANT_ID')
 
 # Function for prompting the assistant
     # Take the thread, word and translation as params
